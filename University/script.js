caf489d1aab31a611965f91b419701ef831885cf
Souhardya Dutta <64770208+Rune-Coder@users.noreply.github.com>
2021-10-31 13:41:14 +0530

    script

--- University/script.js
+++ University/script.js
@@ -1,4 +1,4 @@
-// navbar togglemenu start
+// navbar togglemenu start index.html
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
@@ -9,3 +9,12 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 // navbar togglemenu end
+//date change start blog.html
+var dt = document.getElementById("det");
+let n =  new Date();
+let y = n.getFullYear();
+let m = n.getMonth();
+let d = n.getDate();
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+document.getElementById("det").innerHTML = months[m] + " " + d + ", " + y;
+// date change end
